4f16a284d5b579b744f64412b45ebc02773c11b1
din <20040401dd@gmail.com>
2025-08-20 17:07:40 +0500

    add schemas

---
+++ src/apps/PizzaRestaurant/schemas.py
@@ -0,0 +1,93 @@
+from typing import List
+
+from ninja import ModelSchema, Field
+
+from src.apps.PizzaRestaurant.models import Chef, Restaurant, Pizza, Ingredient, Review
+
+
+class ChefSchema(ModelSchema):
+    class Config:
+        model = Chef
+        model_fields = ['name']
+
+
+class RestaurantSchema(ModelSchema):
+    class Config:
+        model = Restaurant
+        model_fields = ['name', 'address']
+
+
+class ChefInSchema(ChefSchema):
+    restaurant_id: int
+
+
+class ChefNestedSchema(ChefSchema):
+    id: int
+
+
+class RestaurantOutSchema(RestaurantSchema):
+    id: int
+    chef: ChefNestedSchema | None = None
+
+
+class RestaurantNestedSchema(RestaurantSchema):
+    id: int
+
+
+class ChefOutSchema(ChefSchema):
+    id: int
+    restaurant: RestaurantNestedSchema | None = None
+
+
+class IngredientSchema(ModelSchema):
+    class Config:
+        model = Ingredient
+        model_fields = ['name']
+
+
+class IngredientOutSchema(IngredientSchema):
+    id: int
+
+
+class PizzaSchema(ModelSchema):
+    class Config:
+        model = Pizza
+        model_fields = ['name', 'cheese', 'thickness', 'secret_ingredient']
+
+
+class PizzaNestedSchema(ModelSchema):
+    class Config:
+        model = Pizza
+        model_fields = ['name', 'cheese', 'thickness']
+
+    secret_ingredient: List[IngredientOutSchema] | None = None
+
+
+class PizzaInSchema(PizzaSchema):
+    restaurant_id: int
+
+
+class PizzaOutSchema(PizzaSchema):
+    id: int
+    restaurant: RestaurantNestedSchema | None = None
+
+
+class ReviewSchema(ModelSchema):
+    rating: int = Field(..., ge=1, le=5)
+
+    class Config:
+        model = Review
+        model_fields = ['feedback']
+
+
+class ReviewOutSchema(ReviewSchema):
+    id: int
+    restaurant: RestaurantNestedSchema | None = None
+
+
+class ReviewInSchema(ReviewSchema):
+    restaurant_id: int
+
+
+class RestaurantMenuSchema(RestaurantOutSchema):
+    pizzas: list[PizzaNestedSchema]
